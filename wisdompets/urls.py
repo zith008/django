dc021a1b6886387cc48f0e998e8f3cad7d55557d
zith008 <89466554+zith008@users.noreply.github.com>
2023-04-18 09:10:58 +0800

    Add files via upload

--- wisdompets/urls.py
+++ wisdompets/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from adoptions import views
+import members.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('adoptions/<int:id>', views.pet_detail, name='pet_detail')
+    path('adoptions/<int:id>', views.pet_detail, name='pet_detail'),
+    path('members/', members.views.members_view, name='members'),
 ]
